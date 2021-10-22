f2aef32054786e16a36284cc5a2310d018f8e885
deepakm14035 <34639761+deepakm14035@users.noreply.github.com>
2021-10-22 09:48:27 -0700

    First commit
    Create table + insert into + select *

---
+++ build.bat
@@ -0,0 +1,12 @@
+
+CALL gcc db.cpp
+
+CALL a "drop table emp4"
+CALL a "create table emp4 (name char(30), age int)"
+
+CALL a "insert into emp4 values ('abcde',22)"
+CALL a "insert into emp4 values ('fghij',23)"
+CALL a "insert into emp4 values ('klmno',24)"
+CALL a "insert into emp4 values ('pqrst',25)"
+CALL a "insert into emp4 values ('kapeed4',26)"
+PAUSE
